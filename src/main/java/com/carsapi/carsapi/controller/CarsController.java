1b2d5baa829ab79f86b434bbb09811285e80f1f1
tiago <tigolimab223@gmail.com>
2023-01-18 15:55:31 -0300

    feature: model and get mapping as well as controller

--- src/main/java/com/carsapi/carsapi/controller/CarsController.java
+++ src/main/java/com/carsapi/carsapi/controller/CarsController.java
@@ -1,16 +1,31 @@
 package com.carsapi.carsapi.controller;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.carsapi.carsapi.dto.CarDTO;
+import com.carsapi.carsapi.model.carModel;
+import com.carsapi.carsapi.repository.carsRepository;
 
 @RestController
 @RequestMapping("/cars")
 public class CarsController {
+
+   @Autowired
+   private carsRepository repository;
+
+   @GetMapping
+   public List<carModel> listAll(){
+      return repository.findAll();
+   }
+
    @PostMapping
    public void create(@RequestBody CarDTO req){
-        System.out.println(req);
+        repository.save(new carModel(req));
    }
 }
