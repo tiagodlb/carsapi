87820572792e1eb4392d26827746aa5ddd8636b7
tiago <tigolimab223@gmail.com>
2023-01-18 15:19:55 -0300

    feature: cars controller and dto

---
+++ src/main/java/com/carsapi/carsapi/CarsapiApplication.java
@@ -0,0 +1,13 @@
+package com.carsapi.carsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarsapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarsapiApplication.class, args);
+	}
+
+}
