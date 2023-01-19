fbdc10629e0fede52d755e1b40c7303e455214d6
tiago <tigolimab223@gmail.com>
2023-01-19 15:01:55 -0300

    feature: crud

--- src/main/java/com/carsapi/carsapi/dto/CarDTO.java
+++ src/main/java/com/carsapi/carsapi/dto/CarDTO.java
@@ -1,3 +1,30 @@
 package com.carsapi.carsapi.dto;
-public record CarDTO(String modelo, String fabricante, String dataFabricacao, double valor, int anoModelo) {    
+
+import java.util.Date;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Positive;
+
+public record CarDTO(
+
+@NotBlank
+String modelo, 
+
+@NotBlank
+String fabricante, 
+
+@Past
+Date dataFabricacao, 
+
+@NotNull
+@Positive
+@Min(1000)
+double valor, 
+
+@NotNull
+int anoModelo
+) {    
 }
